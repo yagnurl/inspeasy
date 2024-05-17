bf544a2ae12f2bc7d83fa7a65bc09e569a7805bc
yagnur <yagnur7@gmail.com>
2024-05-17 22:35:30 +0300

    feat: introduction to react

---
+++ inspeasy-web/src/stories/header.style.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #bf4f74;
+  background: pink;
+`;
+
+export const HeaderBase = styled.h1`
+  width: 100%;
+  max-width: 1300px;
+`;
